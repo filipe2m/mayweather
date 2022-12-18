8d97655473984e248a582e8858e406db8a2904fb
Filipe Mendes <5122694+filipe2m@users.noreply.github.com>
2022-12-18 20:14:33 +0000

    FEATURE: added navigation and routerview

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import NavigationComponent from "./components/NavigationComponent.vue";
 </script>
 
 <template>
-
+    <div class="flex flex-col min-h-screen font-Roboto bg-gradient-to-tl from-sky-900 to-sky-600">
+        <NavigationComponent />
+        <RouterView />
+    </div>
 </template>
