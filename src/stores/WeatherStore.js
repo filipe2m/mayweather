81cc2d9da83f8e6f430da88a64ddceb44b091ba5
Filipe Mendes <filipe-miguel.mendes_external@capgemini.com>
2022-12-19 20:38:24 +0000

    REAFACTOR: only fetch weather if a new city

--- src/stores/WeatherStore.js
+++ src/stores/WeatherStore.js
@@ -4,10 +4,16 @@ import { getWeatherFromApi } from '../services/OpenWeatherService';
 export const useWeatherStore = defineStore('WeatherStore', {
     state: () => ({
         weather: null,
+        currLat: null,
+        currLng: null
     }),
     actions: {
         async getWeatherData(lat, lng) {
+            if(lat === this.currLat && lng === this.currLng)
+                return;
             this.weather = await getWeatherFromApi(lat, lng);
+            this.currLat = lat;
+            this.currLng = lng;
         },
     },
     getters: {
